dec35d5161736c31ba54f53675cc8304d5131a2e
minhyeok08 <ssklas08@gmail.com>
2024-06-27 17:34:26 +0900

    상품 도메인 추가

---
+++ src/main/java/jpabook/jpashop/repository/ItemRepository.java
@@ -0,0 +1,34 @@
+package jpabook.jpashop.repository;
+
+import jakarta.persistence.EntityManager;
+import jpabook.jpashop.domain.item.Item;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class ItemRepository {
+
+    private final EntityManager em;
+
+    public void save(Item item) {   // 여기서 save는 update와 비슷한 개념으로 이해해도됨
+        if(item.getId() == null) {
+            em.persist(item); // 새상품으로봄
+        }
+        else {
+            em.merge(item);
+        }
+    }
+
+    public Item findOne(Long id) {
+        return em.find(Item.class, id);
+    }
+
+    public List<Item> findAll() {
+        return em.createQuery("select i from Item i", Item.class)
+                .getResultList();
+    }
+
+}
